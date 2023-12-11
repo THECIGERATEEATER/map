86d31f0b33b06f39d74e38034fab17473dbcfbc1
THECIGERATEEATER <152208517+THECIGERATEEATER@users.noreply.github.com>
2023-12-11 11:04:15 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -309,6 +309,7 @@ let Player4: Sprite = null
 let Player2: Sprite = null
 let Player1: Sprite = null
 let Arrow: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
 Arrow = sprites.create(img`
     . . . . . . . . 
     b . . . . . . . 
@@ -330,58 +331,58 @@ let Arrow_22 = sprites.create(img`
     . . . . . . . . 
     `, SpriteKind.Projectile_2)
 Player1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . e e e e e e . . . . . 
+    . . . . e e e e e e e e . . . . 
+    . . . . e e d d d d e e . . . . 
+    . . . . e d f d d f d e . . . . 
+    . . . . e d d d d d d e e . . . 
+    . . . . e e d d d d e e e . . . 
+    . . . . . f f f f f f . e e . . 
+    . . . . . d f f f f d . . e . . 
+    . . . . . d f f f f d . . . . . 
+    . . . . . d f f f f d . . . . . 
     . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . . f . . f . . . . . . 
+    . . . . . . f . . f . . . . . . 
     `, SpriteKind.Player)
 Player2 = sprites.create(img`
-    . . . . . f f 4 4 f f . . . . . 
-    . . . . f 5 4 5 5 4 5 f . . . . 
-    . . . f e 4 5 5 5 5 4 e f . . . 
-    . . f b 3 e 4 4 4 4 e 3 b f . . 
-    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
-    . f 3 3 e b 3 e e 3 b e 3 3 f . 
-    . f 3 3 f f e e e e f f 3 3 f . 
-    . f b b f b f e e f b f b b f . 
-    . f b b e 1 f 4 4 f 1 e b b f . 
-    f f b b f 4 4 4 4 4 4 f b b f f 
-    f b b f f f e e e e f f f b b f 
-    . f e e f b d d d d b f e e f . 
-    . . e 4 c d d d d d d c 4 e . . 
-    . . e f b d b d b d b b f e . . 
-    . . . f f 1 d 1 d 1 d f f . . . 
-    . . . . . f f b b f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . . . 5 5 d d d d 5 5 . . . . 
+    . . . . 5 d f d d f d 5 . . . . 
+    . . . . . d f d d f d . . . . . 
+    . . . . . . d d d d . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . d f f f f d . . . . . 
+    . . . . . d f f f f d . . . . . 
+    . . . . . . f . . f . . . . . . 
+    . . . . . . f . . f . . . . . . 
+    . . . . . f f . f f . . . . . . 
+    . . . . . f f . f f . . . . . . 
     `, SpriteKind.Player_2)
 Player4 = sprites.create(img`
-    . . . . . . . . . 1 . . . . . . 
-    . . . . . . 1 1 1 . 1 1 . . . . 
-    . . . . . . 1 . . . . 1 . . . . 
-    . . . . . . 1 1 1 . 1 1 . . . . 
-    . . . . . . . 1 1 1 . . . . . . 
-    . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
-    . . . . . . . . 1 . . . . . . . 
-    . . . . . . . 1 . . . . . . . . 
-    . . . . . . . 1 . . . . . . . . 
-    . . . . . . . 1 1 . . . . . . . 
-    . . . . . . 1 1 1 . . . . . . . 
-    . . . . . . 1 . 1 1 . . . . . . 
-    . . . . . 1 1 . . 1 . . . . . . 
-    . . . . . 1 . . . 1 1 . . . . . 
-    . . . . 1 1 . . . . 1 . . . . . 
-    . . . . 1 . . . . . 1 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 4 4 4 4 . . . . . . 
+    . . . . . 4 4 4 4 4 4 . . . . . 
+    . . . . 4 4 d d d d 4 4 . . . . 
+    . . . . 4 d f d d f d 4 . . . . 
+    . . . . . d d d d d d . . . . . 
+    . . . . . . d d d d . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . . d 2 2 2 2 2 2 d . . . . 
+    . . . . d d 2 2 2 2 d d . . . . 
+    . . . . d d 2 2 2 2 d d . . . . 
+    . . . . d . e . . e . d . . . . 
+    . . . . . . e . . e . . . . . . 
+    . . . . . . e . . e . . . . . . 
+    . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player_4)
 sprites.destroy(Arrow)
 sprites.destroy(Arrow_22)
@@ -433,9 +434,6 @@ P1_b = 9999999
 P1_a = 999999999
 forever(function () {
     Pointer_1.setPosition(Player1.x + P1_b, Player1.y)
-})
-forever(function () {
-	
 })
 forever(function () {
     Pointer_4.setPosition(Player4.x + p4_b, Player4.y)
